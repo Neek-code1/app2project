7448be63e658a9608337385f7104800b3e534ff8
neek-code1 <cunderwoods23@gmail.com>
2026-08-29 11:20:23 -0700

    initalcommit

---
+++ app.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div>
+      <h1>CineFind</h1>
+      <p>Search for your favorite movies</p>
+    </div>
+  );
+}
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(<App />);
